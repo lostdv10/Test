1656c75f40abcac90a990f0d3cdb871720fec459
lostdv10 <2632838733@qq.com>
2023-05-18 21:30:33 +0800

    something i donot know

--- 网页/script/test.js
+++ 网页/script/test.js
@@ -1,17 +1,29 @@
-function pow(a,b) {
-  // return 8; // :) 我们作弊啦！
-  // return a**b;
-  return Math.pow(a,b)
+// function pow(a,b) {
+//   // return 8; // :) 我们作弊啦！
+//   // return a**b;
+//   return Math.pow(a,b)
+// }
+
+// describe("pow", function() {
+
+//   it("2 raised to power 3 is 8", function() {
+//     assert.equal(pow(2, 3), 8);
+//   });
+
+//   it("3 raised to power 4 is 81", function() {
+//     assert.equal(pow(3, 4), 81);
+//   });
+
+// });
+
+export function hi() {
+  alert(`Hello`);
 }
 
-describe("pow", function() {
+export function bye() {
+  alert(`Bye`);
+}
 
-  it("2 raised to power 3 is 8", function() {
-    assert.equal(pow(2, 3), 8);
-  });
-
-  it("3 raised to power 4 is 81", function() {
-    assert.equal(pow(3, 4), 81);
-  });
-
-});
+export default function() {
+  alert("Module loaded (export default)!");
+}
